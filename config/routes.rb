ed209eff318e51428d07b85261cfc0d77ce75a27
webalexj <alexj_ita@yahoo.com>
2023-09-28 17:01:53 +0200

    creata homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  # controller pages
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
