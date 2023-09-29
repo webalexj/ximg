0788e683d3e4aeccc28773a982f2f97f8720dcf0
webalexj <alexj_ita@yahoo.com>
2023-09-29 13:18:19 +0200

    cambiato design login signup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,8 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+//= require jquery
+//= require jquery_ujs
+//= require rails-ujs
 
-//= require materialize
+//= require materialize-sprockets
